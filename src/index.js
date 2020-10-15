6921c86ab54983c028dd6924f1de18e324f98418
Ilya Barachenia <ilya.barachenia@gmail.com>
2020-10-15 12:06:09 +0200

    feat: finish towel sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var x = []
+  if (matrix === undefined) {
+    return x;
+  }
+  for (var i = 0; i < matrix.length; i++) {
+    if (i % 2 !== 0) {
+      var s = matrix[i].sort ((a, b) => b - a);
+      x = x.concat(matrix[i]);
+    } else {
+      x = x.concat(matrix[i]);
+    }
+  }
+  return x;
 }
